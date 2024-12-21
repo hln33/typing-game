1b8b7827afa03d686dd294ef06ba67de19209c23
Harry Nguyen <hln5@sfu.ca>
2024-12-21 14:58:58 -0800

    customize prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { createEffect, createSignal, Index, type Component } from 'solid-js';
+import { createEffect, createSignal, type Component } from "solid-js";
 
-import logo from './logo.svg';
-import TextPrompt from './components/TextPrompt';
+import logo from "./logo.svg";
+import TextPrompt from "./components/TextPrompt";
 
 const App: Component = () => {
-  const [typedText, setTypedText] = createSignal('');
+  const [typedText, setTypedText] = createSignal("");
   createEffect(() => console.log(typedText()));
 
   const handleInput = (event: InputEvent) => {
@@ -14,10 +14,18 @@ const App: Component = () => {
   return (
     <div class="text-center">
       <header class="bg-slate-800 min-h-screen flex flex-col items-center justify-center text-white">
-        <img src={logo} class="logo-spin h-80" alt="logo" />
+        <img
+          src={logo}
+          class="logo-spin h-80"
+          alt="logo"
+        />
 
         <TextPrompt userTypedText={typedText()} />
-        <input type="text" value={typedText()} onInput={handleInput} />
+        <input
+          type="text"
+          value={typedText()}
+          onInput={handleInput}
+        />
       </header>
     </div>
   );
