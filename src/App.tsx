06aeaf6a0dd118c171cba0c776d9fc700a451a2d
Harry Nguyen <hln5@sfu.ca>
2024-12-22 21:23:22 -0800

    render something for incorrect newline input

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,14 @@ import TimeSelect from "./components/TimeSelect";
 const DEFAULT_TIME_LIMIT = 15;
 // const prompt =
 //   "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
-const prompt =
-  "dict_method_1\n = dict(zip(keys_list, values_list))\nprint('hello world!')";
+// const prompt = "print('hey')\nprint('hello world!')";
+const prompt = "p\nprint('hello world!')";
 
 const App: Component = () => {
   const [typedText, setTypedText] = createSignal("");
   const [active, setActive] = createSignal(false);
   const [timeLimit, setTimeLimit] = createSignal(DEFAULT_TIME_LIMIT);
-  const [summaryVisible, setSummaryVisible] = createSignal(true);
+  const [summaryVisible, setSummaryVisible] = createSignal(false);
 
   const handleTextInput = (event: InputEvent) => {
     if (!active()) {
--- src/components/TextPrompt.tsx
+++ src/components/TextPrompt.tsx
@@ -3,15 +3,22 @@ import PauseIcon from "~icons/solar/pause-bold";
 
 const HIDDEN_INPUT_ID = "hidden-input";
 
+const TextColor = {
+  inactive: "text-gray-400",
+  correct: "text-yellow-400",
+  incorrect: "text-rose-500",
+} as const;
+
 const getTextColor = (attempt: string | null, target: string) => {
   if (attempt === null) {
     return "text-gray-400";
   }
+
   console.assert(
     attempt.length === 1 && target.length === 1,
     `should only be comparing single characters -- attempt: ${attempt} target: ${target}`,
   );
-  return attempt === target ? "text-yellow-400" : "text-rose-500";
+  return attempt === target ? TextColor["correct"] : TextColor["incorrect"];
 };
 
 const TextPrompt: Component<{
@@ -23,6 +30,11 @@ const TextPrompt: Component<{
     document.activeElement === document?.getElementById(HIDDEN_INPUT_ID),
   );
 
+  // createEffect(() => {
+  //   console.log(props.userTypedText);
+  //   console.log(props.prompt.split(""));
+  // });
+
   return (
     <div class="relative p-8 pb-40 w-full">
       <div class={`flex flex-wrap gap-1 ${!isFocused() && "blur-sm"}`}>
@@ -30,26 +42,38 @@ const TextPrompt: Component<{
           {(char, index) => {
             const textColor = () =>
               getTextColor(props.userTypedText.at(index) ?? null, char());
+
+            const isIncorrectNewLine = () =>
+              char() === "\n" && textColor() === TextColor["incorrect"];
+
             const isNextChar = () => index === props.userTypedText.length;
 
             return (
-              <div class="relative text-4xl">
-                <span class={`whitespace-pre ${textColor()}`}>{char()}</span>
-                <Show when={isFocused() && isNextChar()}>
-                  <span class="absolute right-full animate-blink text-yellow-400">
-                    |
+              <>
+                <div class={`relative text-3xl`}>
+                  <span class={`whitespace-nowrap ${textColor()}`}>
+                    {isIncorrectNewLine() ? <>\n</> : char()}
                   </span>
+                  <Show when={isFocused() && isNextChar()}>
+                    <span class="absolute right-full animate-blink text-yellow-400">
+                      |
+                    </span>
+                  </Show>
+                </div>
+
+                {/* force a new line in the flexbox */}
+                <Show when={char() === "\n"}>
+                  <span class="invisible w-full" />
                 </Show>
-              </div>
+              </>
             );
           }}
         </Index>
       </div>
 
-      <input
+      <textarea
         id={HIDDEN_INPUT_ID}
         class="absolute z-50 inset-0 size-full opacity-0"
-        type="textArea"
         autocomplete="off"
         value={props.userTypedText}
         onInput={(e: InputEvent) => props.handleInput(e)}
